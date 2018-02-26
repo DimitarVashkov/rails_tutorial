e17fd480b3a736a501ddc2e90bf9b954108ee6e2
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-02-26 20:09:43 +0000

    Removed image option

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,16 +3,6 @@ class Micropost < ApplicationRecord
   # The microposts are ordered by descending order based on creation time by default
   # http://guides.rubyonrails.org/active_record_querying.html#scopes
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate :picture_size
-
-  private
-
-  def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5MB")
-    end
-  end
 end
